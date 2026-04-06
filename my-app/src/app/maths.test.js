3f1bf984344c5ea98313828283df6d6ee844c2d5
Cathal Farrell <b00162757@mytudublin.ie>
2026-04-06 17:44:17 +0100

    Beginning testing

---
+++ my-app/src/app/maths.test.js
@@ -0,0 +1,13 @@
+const { add, subtract } = require('./maths');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
